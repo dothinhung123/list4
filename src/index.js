9e39e9d7d2c8641f55375d9a5d70898d17f1513a
Nhung <54989905+dothinhung123@users.noreply.github.com>
2020-03-10 14:42:54 +0700

    delete file

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import {createStore,applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
-import reducers from './reducers'
-import thunk from 'redux-thunk'
-import App from './components/App'
-ReactDOM.render(<Provider store={createStore(reducers,applyMiddleware(thunk))}><App></App></Provider>,document.querySelector('#root'))
